3c36f02072be0e941efee0b8b3210a06fac5d37e
Ashlynn Hall <hall.ash9@gmail.com>
2025-03-31 17:31:59 -0600

    did the w5 tasks

--- controllers/invController.js
+++ controllers/invController.js
@@ -13,10 +13,22 @@ invCont.buildByClassificationId = async function (req, res, next) {
   let nav = await utilities.getNav()
   const className = data[0].classification_name
   res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
+    title: className + " vehicles", nav, grid,
   })
 }
+
+/* ***************************
+ *  Build item view
+ * ************************** */
+invCont.buildByInvId = async function (req, res, next) {
+  const item_id = req.params.invId;
+  const data = await invModel.getItemByInvId(item_id);
+  const details = await utilities.buildItemView(data);
+  let nav = await utilities.getNav();
+  const itemName = `${data.inv_year} ${data.inv_make} ${data.inv_model}`;
+  res.render("./inventory/detail", {
+    title: itemName, nav, details,
+  });
+};
 
 module.exports = invCont
